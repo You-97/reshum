e49b0552a270a59da4a90121589b0bb90cc9d1f4
You-97 <youssefelgourari97@gmail.com>
2022-11-10 23:49:21 +0100

    edit

---
+++ src/main/java/com/stoore/StooreApplication.java
@@ -0,0 +1,13 @@
+package com.stoore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StooreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StooreApplication.class, args);
+    }
+
+}
